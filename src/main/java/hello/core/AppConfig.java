df1354a801ba9def5e9e21af5dac071b35b5832f
ejcho <ejcho.dev@gmail.com>
2022-05-26 01:18:26 +0900

    관심사의 분리

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,25 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+/**
+ * 애플리케이션의 전체 동작 방식을 구성(config)하기 위해, 구현 객체를 생성하고, 연결하는 책임을 가지는
+ * 별도의 설정 클래스
+ */
+public class AppConfig {
+
+    public MemberService memberService() {
+        // 생성자 주입: 생성자를 통해 구현 객체를 생성하여 주입한다
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+
+}
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,13 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        // MemberServiceImpl 입장에서 보면 의존관계를 마치 외부에서 주입해주는 것 같다고 해서
+        // DI(Dependency Injection) 우리말로 의존관계 주입 또는 의존성 주입이라고 한다
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
