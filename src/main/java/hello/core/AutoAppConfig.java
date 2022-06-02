f9f0038fe4aa68cad63072558a7d8e81bf0ef03a
ejcho <ejcho.dev@gmail.com>
2022-06-02 19:02:40 +0900

    옵션 처리

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -15,8 +15,10 @@ public class AutoAppConfig {
 
     // Overriding bean definition for bean 'memoryMemberRepository' with a different definition: replacing
     // 자동 빈 vs 수동 빈 --> 수동 빈이 자동 빈을 오버라이딩 한다
+    /*
     @Bean(name = "memoryMemberRepository")
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+    */
 }
