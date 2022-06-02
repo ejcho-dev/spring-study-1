4e6df19517bc1344e54b4095555868830c660d38
ejcho <ejcho.dev@gmail.com>
2022-06-02 18:46:22 +0900

    다양한 의존관계 주입 방법

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,12 +12,47 @@ public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
+//    @Autowired // 생성자가 하나일 때는 @Autowired 생략 가능, 생성자 주입: 불변&필수값, 필드 final
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
 
+    /*
+    // 수정자 주입 방식: 선택&변경 가능성이 있는 의존관계에 사용, @Autowired(required = false)로 필수값 없이 사용가능
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+    */
+    
+    /*
+    // 필드 주입 방식: 권장하지 않음. setter를 추가하지 않으면 순수 자바 코드로 테스트가 어려움 (의존성 주입을 해줘야 함)
+    // @SpringBootTest 사용하는 테스트 코드 작성 시에만 사용하기를 권장
+    @Autowired private MemberRepository memberRepository;
+    @Autowired private DiscountPolicy discountPolicy;
+    */
+
+    /*
+    // 일반 메서드 주입 방식: 잘 사용하지 않음
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    @Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+    */
+    
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
