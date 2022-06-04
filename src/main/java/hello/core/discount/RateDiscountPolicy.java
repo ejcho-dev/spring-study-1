53bad7c069212d88fd4dd67866d522d44503acf6
ejcho <ejcho.dev@gmail.com>
2022-06-04 19:38:08 +0900

    자동, 수동의 올바른 실무 운영 기준

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 //@Qualifier("mainDiscountPolicy")
-@MainDiscountPolicy // @Qualifier 사용할 때의 오류(예: 오타)를 방지 --> 애노테이션 직접 정의
+@MainDiscountPolicy // @Qualifier 사용할 때의 오류(예: 오타)를 방지 --> 애노테이션 직접 정의@
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
