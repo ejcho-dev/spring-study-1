dde9f9a875b4524c592fce75da57de6de540cc8b
ejcho <ejcho.dev@gmail.com>
2022-06-05 19:38:59 +0900

    스코프와 프록시

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +9,11 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")
+// 적용대상이 클래스면 ScopedProxyMode.TARGET_CLASS
+// 적용대상이 인터페이스면 ScopedProxyMode.INTERFACES
+// CGLIB라는 라이브러리로 MyLogger를 상속 받은 가짜 프록시 객체를 만들어서 주입
+// 가짜 프록시 객체는 요청이 오면 그때 내부에서 진짜 빈을 요청하는 위임 로직이 포함되어있다
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MyLogger {
 
     private String uuid;
